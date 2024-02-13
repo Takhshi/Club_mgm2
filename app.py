ee19adffede47c9c2851d639354e5d694ddb50e3
h.takahashi <h.takahashi.sys22@morijyobi.ac.jp>
2024-02-13 12:22:55 +0900

    URL変更

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def get_connection():
 @app.route('/')
 def index():
     club_list = club.club_list()
-    return render_template('club_create/club_not_create_exe.html', club_list=club_list)
+    return render_template('top/top.html', club_list=club_list)
 
 @app.route('/backtop_s', methods=['post'])
 def backtop_student():
